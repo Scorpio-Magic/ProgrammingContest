d65ef8b4ec3cff435a493858b46f71b294c672cf
Zhijun An <anzhijun@stu.pku.edu.cn>
2023-07-20 15:31:11 +0800

    add acwing_792 high-precision substraction

---
+++ acwing/acwing_base/acwing_792.java
@@ -0,0 +1,66 @@
+/*
+    Author: PeterAn
+    Date: 2023-07-20
+    Algorithm: high-precision subtraction
+*/
+
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class acwing_792 {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+
+        AcWing solver = new AcWing();
+        solver.solve(in, out);
+        
+        out.close();
+    }
+
+    static class AcWing {
+        public void solve(InputReader in, PrintWriter out) {
+            BigInteger a = in.nextBigInteger();
+            BigInteger b = in.nextBigInteger();
+            out.println(a.subtract(b).toString());
+        }
+    }
+
+    static class InputReader {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream));
+            tokenizer = new StringTokenizer("");
+        }
+        private String innerNextLine() {
+            try {
+                return reader.readLine();
+            } catch (IOException ex) {
+                return null;
+            }
+        }
+        public boolean hasNext() {
+            while (!tokenizer.hasMoreTokens()) {
+                String nextLine = innerNextLine();
+                if (nextLine == null) return false;
+                tokenizer = new StringTokenizer(nextLine);
+            }
+            return true;
+        }
+        public String next() {
+            if (hasNext()) return tokenizer.nextToken();
+            return null;
+        }
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        public BigInteger nextBigInteger() {
+            return new BigInteger(next());
+        }
+    }
+}
